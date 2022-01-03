c6ab643b0219d78299b5fdc7b5cca04dc1916370
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-01-03 16:15:22 -0600

    Created get route for all users

--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,14 @@ const db = require('../db')
 const router = new Router()
 
 module.exports = router
+
+// Local - GET ALL
+router.get('/', async(req, res)=>{
+    try {
+        const result = await db.query('SELECT * FROM users')
+        res.send(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.send(`Error ${err}`);
+    }
+});
